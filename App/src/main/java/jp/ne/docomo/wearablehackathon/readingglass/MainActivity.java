80b52925fda3bf6c1ec3353708cda10ecd81e55c
Masahiro Wakame <vvakame@gmail.com>
2013-12-07 14:56:48 +0900

    Ok, glass. Use a reading glass で起動できるようにした

--- App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/MainActivity.java
+++ App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/MainActivity.java
@@ -25,27 +25,6 @@ public class MainActivity extends Activity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -56,10 +35,9 @@ public class MainActivity extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
     }
-
 }
