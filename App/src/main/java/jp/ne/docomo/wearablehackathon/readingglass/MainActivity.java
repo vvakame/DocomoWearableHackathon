9ab1f9c6f30ddab54ebeb8a0b76ced4dfc7d02ff
Masahiro Wakame <vvakame@gmail.com>
2013-12-07 15:44:25 +0900

    build.gradle の設定がSneakPeak利用じゃなかったので利用するように変更, Card クラスを試しに使ってみた

--- App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/MainActivity.java
+++ App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/MainActivity.java
@@ -1,43 +1,20 @@
 package jp.ne.docomo.wearablehackathon.readingglass;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import com.google.android.glass.app.Card;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        Card card = new Card(this);
+        card.setText("やばい");
+        card.setInfo("すごい");
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
+        setContentView(card.toView());
     }
 }
