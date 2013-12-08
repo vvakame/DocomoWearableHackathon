a52939156a1ef434c66ab2ee54b558ec82e684d5
Masahiro Wakame <vvakame@gmail.com>
2013-12-08 16:10:11 +0900

    Zoomの操作ができるようにした

--- App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/ReadingGlassActivity.java
+++ App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/ReadingGlassActivity.java
@@ -1,11 +1,16 @@
 package jp.ne.docomo.wearablehackathon.readingglass;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,9 +23,18 @@ public class ReadingGlassActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // https://code.google.com/p/google-glass-api/issues/detail?id=259
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         SurfaceView surfaceView = new SurfaceView(this);
         SurfaceHolder holder = surfaceView.getHolder();
         holder.addCallback(mSurfaceListener);
+
+        mGestureDetector = createGestureDetector(this);
 
         setContentView(surfaceView);
     }
@@ -30,6 +44,8 @@ public class ReadingGlassActivity extends Activity {
     Camera mCamera;
 
     SurfaceHolder.Callback mSurfaceListener = new MySurfaceCallback();
+
+    GestureDetector mGestureDetector;
 
     @Override
     protected void onResume() {
@@ -127,5 +143,47 @@ public class ReadingGlassActivity extends Activity {
 
             mHolder = null;
         }
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                final int zoomDelta = 12;
+                if (gesture == Gesture.TAP) {
+                    Log.d(TAG, "onGesture tap");
+                    // do something on tap
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    Log.d(TAG, "onGesture two tap");
+                    // do something on two finger tap
+                    return true;
+                } else if (gesture == Gesture.SWIPE_RIGHT) {
+                    Log.d(TAG, "onGesture swite right");
+                    Camera.Parameters parameters = mCamera.getParameters();
+                    parameters.setZoom(Math.min(parameters.getMaxZoom(), parameters.getZoom() + zoomDelta));
+                    mCamera.setParameters(parameters);
+                    return true;
+                } else if (gesture == Gesture.SWIPE_LEFT) {
+                    Log.d(TAG, "onGesture swite left");
+                    Camera.Parameters parameters = mCamera.getParameters();
+                    parameters.setZoom(Math.max(1, parameters.getZoom() - zoomDelta));
+                    mCamera.setParameters(parameters);
+                    return true;
+                }
+                return false;
+            }
+        });
+        return gestureDetector;
+    }
+
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 }
