cd3c091a56e46db912b666d91488cfbb746808eb
Masahiro Wakame <vvakame@gmail.com>
2013-12-08 14:27:03 +0900

    カメラ周りの安定度を高めたい…

--- App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/ReadingGlassDrawer.java
+++ App/src/main/java/jp/ne/docomo/wearablehackathon/readingglass/ReadingGlassDrawer.java
@@ -14,36 +14,14 @@ public class ReadingGlassDrawer implements LiveCardCallback {
     static final String TAG = ReadingGlassDrawer.class.getSimpleName();
 
     private SurfaceHolder mHolder;
-    private boolean mPaused;
 
     Camera mCamera;
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        Log.d(TAG, "surfaceChanged, w=" + width + ", h=" + height);
-
-        mCamera.stopPreview();
-
-        Camera.Parameters parameters = mCamera.getParameters();
-        List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
-        Camera.Size applySize = sizes.get(0);
-        for (Camera.Size size : sizes) {
-            Log.d(TAG, "size=" + size.width + "," + size.height);
-            if (size.width == width && size.height == height) {
-                applySize = size;
-            }
-        }
-        Log.d(TAG, "use size=" + applySize.width + "," + applySize.height);
-        // http://stackoverflow.com/questions/19235477/google-glass-preview-image-scrambled-with-new-xe10-release
-        parameters.setPreviewFpsRange(30000, 30000);
-        parameters.setPreviewSize(applySize.width, applySize.height);
-
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-    }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
+        Log.d(TAG, "surfaceCreated");
+
         mHolder = holder;
 
         Log.d(TAG, "Has " + Camera.getNumberOfCameras() + " camera.");
@@ -63,18 +41,85 @@ public class ReadingGlassDrawer implements LiveCardCallback {
         }
     }
 
+    int mFormat = -1;
+    int mWidth = -1;
+    int mHeight = -1;
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d(TAG, "surfaceChanged, format=" + format + ", w=" + width + ", h=" + height);
+        mHolder = holder;
+        mFormat = format;
+        mWidth = width;
+        mHeight = height;
+
+        if (mCamera == null) {
+            mCamera = Camera.open(0);
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+            } catch (IOException e) {
+                Log.e(TAG, "an error occured!", e);
+                mCamera.release();
+                mCamera = null;
+                return;
+            }
+        }
+        mCamera.stopPreview();
+
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+        Camera.Size applySize = sizes.get(0);
+        for (Camera.Size size : sizes) {
+            Log.d(TAG, "size=" + size.width + "," + size.height);
+            if (size.width == width && size.height == height) {
+                applySize = size;
+            }
+        }
+        Log.d(TAG, "use size=" + applySize.width + "," + applySize.height);
+        // http://stackoverflow.com/questions/19235477/google-glass-preview-image-scrambled-with-new-xe10-release
+        parameters.setPreviewFpsRange(30000, 30000);
+        Log.d(TAG, "isZoomSupported:" + parameters.isZoomSupported());
+        Log.d(TAG, "getMaxZoom:" + parameters.getMaxZoom());
+        parameters.setZoom(parameters.getMaxZoom());
+        parameters.setPreviewSize(applySize.width, applySize.height);
+
+        mCamera.setParameters(parameters);
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+        } catch (IOException e) {
+            Log.e(TAG, "an error occured!", e);
+            mCamera.release();
+            mCamera = null;
+            return;
+        }
+        mCamera.startPreview();
+        }
+
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        mCamera.stopPreview();
-        mCamera.release();
-        mCamera = null;
+        Log.d(TAG, "surfaceDestroyed");
+
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
 
         mHolder = null;
     }
 
     @Override
     public void renderingPaused(SurfaceHolder holder, boolean paused) {
-        mPaused = paused;
-        mCamera.stopPreview();
+        Log.d(TAG, "renderingPaused paused=" + paused);
+
+        if (paused) {
+            if (mCamera != null) {
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            }
+        } else {
+            surfaceChanged(holder, mFormat, mWidth, mHeight);
+        }
     }
 }
